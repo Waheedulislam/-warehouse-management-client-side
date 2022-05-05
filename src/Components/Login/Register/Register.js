c9305b7349cede55b36085c744e9674aac67b7b6
Whaeedil <baoppyhossen1234@gmail.com>
2022-05-05 16:50:40 +0600

    register-from

--- src/Components/Login/Register/Register.js
+++ src/Components/Login/Register/Register.js
@@ -1,10 +1,29 @@
-import React from 'react';
-
+import { Link, useNavigate } from 'react-router-dom';
+import './Register.css'
 const Register = () => {
+    const navigate = useNavigate();
+
+    const navigateLogin = () => {
+        navigate('/login')
+    };
+
+
+    const handleRegister = event => {
+        event.preventDefault();
+    }
+
     return (
-        <div>
-            <h1>This is Register</h1>
+        <div className='register-form'>
+            <h1 className='text-center'>This is Register</h1>
+            <from onSubmit={handleRegister}>
+                <input type="text" name="name" id="" placeholder='Your Name' required />
+                <input type="email" name="email" id="" placeholder='Email Address' required />
+                <input type="password" name="password" id="" placeholder='Password' required />
+                <input type="submit" value="Register" />
+            </from>
+            <h6>Already have an account ? <Link to='/login' className='text-danger pe-auto  text-decoration-none' onClick={navigateLogin}>Please Login</Link></h6>
         </div>
+
     );
 };
 
