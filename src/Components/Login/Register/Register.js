aa8463a9357111fd2554411d8f9a7042571f94d5
Whaeedil <baoppyhossen1234@gmail.com>
2022-05-05 17:21:14 +0600

    set-env-variablw-firebase

--- src/Components/Login/Register/Register.js
+++ src/Components/Login/Register/Register.js
@@ -10,18 +10,26 @@ const Register = () => {
 
     const handleRegister = event => {
         event.preventDefault();
+        // console.log(event.target.name);
+        const name = event.target.name.value;
+        const password = event.target.password.value;
+        const email = event.target.email.value;
+        console.log(name, password, email);
     }
 
     return (
         <div className='register-form'>
             <h1 className='text-center'>This is Register</h1>
-            <from onSubmit={handleRegister}>
+
+
+            <form onSubmit={handleRegister}>
                 <input type="text" name="name" id="" placeholder='Your Name' required />
                 <input type="email" name="email" id="" placeholder='Email Address' required />
                 <input type="password" name="password" id="" placeholder='Password' required />
                 <input type="submit" value="Register" />
-            </from>
-            <h6>Already have an account ? <Link to='/login' className='text-danger pe-auto  text-decoration-none' onClick={navigateLogin}>Please Login</Link></h6>
+                <h6>Already have an account ? <Link to='/login' className='text-danger pe-auto  text-decoration-none' onClick={navigateLogin}>Please Login</Link></h6>
+            </form>
+
         </div>
 
     );
