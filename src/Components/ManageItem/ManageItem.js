e3c53fca1c9b63de98587ebbdbb1f05193083d04
Whaeedil <baoppyhossen1234@gmail.com>
2022-05-07 13:12:06 +0600

    table-res

--- src/Components/ManageItem/ManageItem.js
+++ src/Components/ManageItem/ManageItem.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { Button, Table } from 'react-bootstrap';
+import { Button, Container, Table } from 'react-bootstrap';
+import './ManageItem.css'
 
 const ManageItem = () => {
     const [items, setItems] = useState([]);
@@ -14,35 +15,39 @@ const ManageItem = () => {
     }, [])
     return (
         <div>
-            <Table className='container' striped bordered hover size="sm">
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Images</th>
-                        <th>Price</th>
-                        <th>Quantity</th>
-                    </tr>
-                </thead>
-                <tbody style={{ width: '120px' }}>
-                    {
-                        items.map(item => {
-                            return <tr key={item._id}>
-                                <td style={{ width: '130px' }} > <h4>{item.name}</h4> </td>
-                                <td style={{ width: '130px' }}>
-                                    <img style={{ width: '130px' }} src={item.img} alt="" />
-                                </td>
-                                <td style={{ width: '130px' }}> <h6>{item.price}</h6> </td>
-                                <td style={{ width: '130px' }}> <h6>{item.quantity}</h6> </td>
-                                <td style={{ width: '130px' }}>
-                                    <Button className='btn btn-danger'>Delete</Button>
-                                    <br />
-                                    <Button className='btn btn-success'>Edit</Button>
-                                </td>
-                            </tr>
-                        })
-                    }
-                </tbody>
-            </Table>
+            <div>
+                <Table striped bordered hover size="sm" responsive>
+                    <thead className='tdt-res'>
+                        <tr>
+                            <th>Name</th>
+                            <h5 ><small>Images</small></h5>
+                            <th>Price</th>
+                            <th>Quantity</th>
+                            <th>Update</th>
+                        </tr>
+                    </thead>
+                    <tbody className='td-res' style={{ width: '120px' }}>
+                        {
+                            items.map(item => {
+                                return <tr key={item._id}>
+                                    <td style={{ width: '130px' }} > <h4>{item.name}</h4> </td>
+                                    <td style={{ width: '100px' }}>
+                                        <img style={{ width: '220px' }} src={item.img} alt="" />
+                                    </td>
+                                    <td style={{ width: '100px' }}> <h4>{item.price}</h4> </td>
+                                    <td style={{ width: '100px' }}> <h4>{item.quantity}</h4> </td>
+                                    <td style={{ width: '80px' }}>
+                                        <Button className='btn btn-danger'>Delete</Button>
+                                        <br />
+                                        <br />
+                                        <Button style={{ width: '74px' }} className='btn btn-success'>Edit</Button>
+                                    </td>
+                                </tr>
+                            })
+                        }
+                    </tbody>
+                </Table>
+            </div>
 
         </div>
     );
