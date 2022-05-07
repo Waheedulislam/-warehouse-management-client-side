448cacff84f9d901d8f3de444ed5f19c938df7c8
Whaeedil <baoppyhossen1234@gmail.com>
2022-05-07 21:50:17 +0600

    done

--- src/Components/AddItem/AddItem.js
+++ src/Components/AddItem/AddItem.js
@@ -27,50 +27,35 @@ const AddItem = () => {
             .then(res => res.json())
             .then(res => {
                 console.log(res);
-                toast('Successful Item added')
+                alert('Successful Item added')
                 event.target.reset();
             })
-    }
-    // const onSubmit = data => {
-    //     const order = {
-    //         email: user.email
-    //     }
-    //     const url = `http://localhost:5000/item?email=${order}`;
-    //     fetch(url, {
-    //         method: 'POST',
-    //         headers: {
-    //             'content-type': 'application/json'
-    //         },
-    //         body: JSON.stringify(data)
-    //     })
-    //         .then(res => res.json())
-    //         .then(result => {
-    //             console.log(result);
-    //         })
-    // };
-
+    };
 
     return (
-        <div>
-            <h1>add item</h1>
+        <div className='mt-5'>
             <form onSubmit={handleNewItem} className="text-center">
-                <input required className='w-50 mb-2' type="text" name="name" placeholder='Name' id="" />
-                <br />
-
-                <input required className='w-50 mb-2' type="text" name="img" placeholder='photo URL' id="" />
-                <br />
-
-                <input required className='w-50 mb-2' type="text" name="price" placeholder='Price' id="" />
-                <br />
-                <input required className='w-50 mb-2' type="text" name='quantity' placeholder='quantity' />
-                <br />
+                <h1>Add Product</h1>
                 <input required className='w-50 mb-2' type="email" value={user.email} readOnly disabled name="email" id="" placeholder='email' />
                 <br />
-                <input required className='w-50 mb-2' type="text" name="description" placeholder='shortDescriptions' id="" />
+
+                <input required autoComplete='off' className='w-50 mb-2' type="text" name="name" placeholder='Name' id="" />
                 <br />
+
+                <textarea required autoComplete='off' className='w-50 mb-2' type="text" name="description" placeholder='shortDescriptions' id="" />
+                <br />
+
+                <input required autoComplete='off' className='w-50 mb-2' type="text" name="price" placeholder='Price' id="" />
+                <br />
+                <input required autoComplete='off' className='w-50 mb-2' type="text" name='quantity' placeholder='quantity' />
+                <br />
+                <input required autoComplete='off' className='w-50 mb-2' type="text" name="img" placeholder='photo URL' id="" />
+                <br />
+
+
                 <input type="submit" value="Add Item" />
             </form>
-        </div>
+        </div >
     );
 };
 
--- src/Components/MyItems/MyItems.js
+++ src/Components/MyItems/MyItems.js
@@ -1,15 +1,62 @@
+import { Button, Table } from 'react-bootstrap';
 import useItems from '../../Hooks/useItems';
 
 const MyItems = () => {
     const [items, setItems] = useItems();
+    const handleDelete = id => {
+        const proceed = window.confirm('Are you sure want to delete ?')
+        if (proceed) {
+            const url = `http://localhost:5000/item/${id}`
+            fetch(url, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data);
+                    const uiDelete = items.filter(item => item._id !== id);
+                    setItems(uiDelete);
+                })
+        }
+    }
     return (
         <div>
-            <h1 className='text-center text-primary'>My Items</h1>
-            {
-                items.map(item => <div key={item._id}>
-                    <h4>{item.name}</h4>
-                </div>)
-            }
+            <h1 className='text-center text-dark'><span className='text-success'>MY</span> <span className='text-danger'>Add </span><span className='text-success'>Items</span></h1>
+            <div>
+                <Table className='container mt-2' striped bordered hover size="sm" responsive>
+                    <thead className='tdt-res'>
+                        <tr>
+                            <th><h4>Name</h4></th>
+                            <th><h5 ><small>Images</small></h5></th>
+                            <th><h4>Price</h4></th>
+                            <th><h4>Quantity</h4></th>
+                            <th><h4>Update</h4></th>
+                        </tr>
+                    </thead>
+                    <tbody className='td-res' style={{ width: '120px' }}>
+                        {
+                            items.map(item => {
+                                if (item.email) {
+                                    return <tr key={item._id}>
+                                        <td style={{ width: '130px' }} > <h4>{item.name}</h4> </td>
+                                        <td style={{ width: '100px' }}>
+                                            <img style={{ width: '220px' }} src={item.img} alt="" />
+                                        </td>
+                                        <td style={{ width: '100px' }}> <h4>{item.price}</h4> </td>
+                                        <td style={{ width: '100px' }}> <h4>{item.quantity}</h4> </td>
+                                        <td style={{ width: '80px' }}>
+
+                                            <Button onClick={() => handleDelete(item._id)} className='btn btn-danger'>Delete</Button>
+                                            <br />
+                                            <br />
+                                            <Button style={{ width: '74px' }} className='btn btn-success'>Edit</Button>
+                                        </td>
+                                    </tr>
+                                }
+                            })
+                        }
+                    </tbody>
+                </Table>
+            </div>
 
         </div>
     );
