cd7f3c2992f189b1699a42dd6af767e0b588e60b
Whaeedil <baoppyhossen1234@gmail.com>
2022-05-08 10:48:45 +0600

    delevired-done

--- src/Components/shared/Footer/Footer.js
+++ src/Components/shared/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer className=' mt-5' class="text-light bg-dark" style={{ height: '65px' }}>
+        <footer className=' mt-5 text-light bg-dark' style={{ height: '65px' }}>
             <p className='text-white font h6 text-center pt-4'><small>Copyright © {(new Date().getFullYear())} bikeway.com</small></p>
         </footer>
     );
--- src/Inventory/Inventory.js
+++ src/Inventory/Inventory.js
@@ -1,28 +1,67 @@
+import { async } from '@firebase/util';
+import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { Button, Card } from 'react-bootstrap';
 import { Link, useParams } from 'react-router-dom';
-import Banner from '../../src/img/bike/banner2 (2).png'
 import './inventory.css'
 
 const Inventory = () => {
     const { inventoryId } = useParams();
-    const [item, setItem] = useState([]);
-    const [product, setProduct] = useState({});
-    const { quantity } = product;
+    const [item, setItem] = useState({});
+    const [quantity, setQuantity] = useState({});
+
+
+
+    const handleDelivered = (quantity) => {
+
+        try {
+            const newQuantity = quantity - 1;
+            const url = `http://localhost:5000/item/${inventoryId}`
+
+            const getData = async () => {
+                const response = await axios.put(url, { newQuantity })
+                setItem(response.data);
+            }
+            getData();
+        } catch {
+
+        }
+    }
+
+    const handleQuantitySubmit = e => {
+        e.preventDefault();
+        try {
+            const url = `http://localhost:5000/addQuantity/${inventoryId}?oldQuantity=${item.quantity}`;
+
+            const updateQuantity = async () => {
+                const response = await axios.put(url, { quantity })
+                console.log(response.data.massage);
+            }
+            updateQuantity();
+        } catch {
+
+        }
+    }
+
 
     useEffect(() => {
-        const url = `http://localhost:5000/item/${inventoryId}`;
-        fetch(url)
-            .then(res => res.json())
-            .then(data => setItem(data))
-    }, [])
 
-    const addNewQuantity = event => {
-        event.preventDefault();
-        const quantity = event.target.number.value;
-        console.log(quantity);
-        event.target.reset();
-    }
+        try {
+            const url = `http://localhost:5000/itemDetails/${inventoryId}`;
+
+            const add = async () => {
+                const response = await axios.get(url)
+                setItem(response.data)
+            }
+            add();
+        } catch {
+
+        }
+
+    }, [item])
+
+
+
     return (
         <div className='d-flex w-100'>
             <div className='mx-auto pt-2 card-style' >
@@ -45,17 +84,18 @@ const Inventory = () => {
                                 <h5 className='text-center ps-2'>Price:</h5> <h6 className=' ps-1 pe-2'>{item.price}</h6>
                             </div>
                             <div className='d-flex justify-content-center'>
-                                <h4 className='text-center ps-1 pb-1'>Quantity:</h4> <h5 className='pt-1 ps-1'>{item.quantity}</h5>
+                                <h4 className='text-center ps-1 pb-1'>Quantity:</h4> <h5 className='pt-1 ps-1' id="total-quantity">{item.quantity}</h5>
                             </div>
 
 
-                            <button className='btn btn-danger'>Delivered</button>
+                            <button onClick={() => handleDelivered(item.quantity)} className='btn btn-danger'>Delivered</button>
                         </div>
                         <hr />
                         <div className='text-center'>
                             <h4>Add a Quantity</h4>
-                            <form onSubmit={addNewQuantity}>
-                                <input onBlur={quantity} required placeholder='Increase Quantity' type="number" name="number" id="" />
+
+                            <form onSubmit={handleQuantitySubmit}>
+                                <input required onBlur={(e) => setQuantity(e.target.value)} placeholder='Increase Quantity' type="number" name="number" id="" />
                                 <br />
                                 <br />
                                 <Button type='submit' className='btn btn-success'>Increase quantity</Button>
